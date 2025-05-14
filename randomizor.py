0e43bf0a3145aca1cfe9be532c06b9e3df1aca09
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2025-05-14 09:24:07 +0900

    Update randomizor.py
    그냥 function으로 바꿈

--- randomizor.py
+++ randomizor.py
@@ -1,7 +1,7 @@
 
 import random as r
 
-def shuffle(wstring):
+def stringshuffle(wstring):
     wlist = wstring.split(", ")
     r_wlist = []
     ovlp_prevent = []
@@ -17,12 +17,29 @@ def shuffle(wstring):
 
     return r_wlist
 
+def listshuffle(wlist):
+    r_wlist = []
+    ovlp_prevent = []
+
+    for i in range(len(wlist)):
+        ovlp_prevent.append(i)
+
+    # shuffle
+    for j in range(len(wlist)):
+        choice = r.choice(ovlp_prevent)
+        ovlp_prevent.remove(choice)
+        r_wlist.append(wlist[choice])
+
+    return r_wlist
+
+'''
 # print
 inputstr = input("Input: ")
-r_wlist = shuffle(inputstr)
+r_wlist = stringshuffle(inputstr)
 print("\n\nText Output: ")
 for k in range(len(r_wlist)):
     if k == len(r_wlist)-1:
         print(r_wlist[k])
     else:
         print(r_wlist[k], end=", ")
+'''
